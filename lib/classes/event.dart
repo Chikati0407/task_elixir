bdc2da1fb2e2c2408fc41b3bde3e3e59d7be617d
Chikati0407 <130910609+Chikati0407@users.noreply.github.com>
2025-01-26 00:27:25 +0900

    providerとカレンダーの色決定

--- lib/classes/event.dart
+++ lib/classes/event.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'event.freezed.dart';
@@ -21,6 +20,21 @@ class Event with _$Event {
   factory Event.fromJson(Map<String, Object?> json)
   => _$EventFromJson(json);
 }
+
+/* TODO 色対応をしておく
+0 カレンダーの色
+1 ラベンダー  #7986CB rgb(121, 134, 203)
+2 セージ     #33B679 rgb(51, 182, 121)
+3 ブドウ     #8E24AA rgb(142, 36, 170)
+4 フラミンゴ  #E67C73 rgb(230, 124, 115)
+5 バナナ     #F6BF26 rgb(246, 191, 38)
+6 ミカン     #F4511E rgb(244, 81, 30)
+7 ピーコック  #039BE5 rgb(3, 155, 229)
+8 グラファイト #616161 rgb(97, 97, 97)
+9 ブルーベリー #3F51B5 rgb(63, 81, 181)
+10 バジル     #0B8043 rgb(11, 128, 67)
+11 トマト     #D50000 rgb(213, 0, 0)
+*/
 
 
 // @freezed
--- lib/state/appstate.dart
+++ lib/state/appstate.dart
@@ -1,5 +1,35 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:task_elixir/classes/event.dart';
 
-final currentDateProvider = StateProvider((ref){
+final currentDateProvider = StateProvider<DateTime>((ref){
   return DateTime.now();
+});
+
+
+
+final eventsProvider = FutureProvider((ref){
+  return Future.value(
+    [
+      Event(
+        id: '1',
+        htmlLink: 'http://example.com',
+        summary: 'Meeting',
+        description: 'Project meeting',
+        location: 'Conference Room',
+        ColorId: '1',
+        startTime: DateTime.now(),
+        endTime: DateTime.now().add(Duration(hours: 1)),
+      ),
+      Event(
+        id: '2',
+        htmlLink: 'http://example.com',
+        summary: 'Lunch',
+        description: 'Team lunch',
+        location: 'Cafeteria',
+        ColorId: '2',
+        startTime: DateTime.now().add(Duration(hours: 2)),
+        endTime: DateTime.now().add(Duration(hours: 3)),
+      ),
+    ]
+  );
 });
